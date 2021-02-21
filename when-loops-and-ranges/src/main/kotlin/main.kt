42a480c8ca0ee62bbf7cd729d125ec04f6c64d6a
robomajor <robokajor@gmail.com>
2021-02-21 22:01:40 +0100

    Class about 'when'

--- when-loops-and-ranges/src/main/kotlin/main.kt
+++ when-loops-and-ranges/src/main/kotlin/main.kt
@@ -1,19 +1,19 @@
 fun main() {
-    cases("Hello")
-    cases(1)
-    cases(0L)
-    cases(MyClass())
-    cases("hello")
-}
 
-fun cases(obj: Any) {
-    when (obj) {                                     // 1
-        1 -> println("One")                          // 2
-        "Hello" -> println("Greeting")               // 3
-        is Long -> println("Long")                   // 4
-        !is String -> println("Not a string")        // 5
-        else -> println("Unknown")                   // 6
-    }
+    val whenClass = When()
+
+    whenClass.cases("Hello")
+    whenClass.cases(1)
+    whenClass.cases(0L)
+    whenClass.cases(MyClass())
+    whenClass.cases("hello")
+
+    println()
+
+    println(whenClass.whenAssign("Hello"))
+    println(whenClass.whenAssign(3.4))
+    println(whenClass.whenAssign(1))
+    println(whenClass.whenAssign(MyClass()))
 }
 
 class MyClass
